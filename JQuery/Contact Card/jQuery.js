d79328b1a9163fc15f45b13a101d743a6ac8ddff
jtran20 <jerrytran20@gmail.com>
2016-11-25 16:28:34 -0600

    JQuery update

--- JQuery/Contact Card/jQuery.js
+++ JQuery/Contact Card/jQuery.js
@@ -12,6 +12,7 @@ $(document).ready(function()
         var $ele = $('<div></div>')
         $ele.html("<h2>" + fields.firstName + " " + fields.lastName + "</h2><p>" + fields.description + "</p>")
         $('.cards').append($ele)
+        $('input:first-child, input:nth-child(2), textarea').val(null)
     });
 
     $("div.cards").on("click", "h2", function()
